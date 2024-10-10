678272016f095e98737e2311a433dea99c496209
gideonadeti <gideonadeti0@gmail.com>
2024-10-10 09:39:24 +0000

    Set up Main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 export default function App() {
   return (
-    <div>
+    <div className="d-flex flex-column vh-100">
       <Header />
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main.tsx
@@ -0,0 +1,10 @@
+export default function Main() {
+  return (
+    <main className="flex-grow-1 container-fluid mt-5">
+      <div
+        className="container border rounded shadow-sm"
+        style={{ maxWidth: "420px", minHeight: "360px" }}
+      ></div>
+    </main>
+  );
+}
